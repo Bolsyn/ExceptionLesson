18d5dbc80bb9f4339815a0b110effe74dd3eaaf3
Bolsyn <Serikagis@yandex.ru>
2020-12-28 21:14:39 +0600

    Add project files.

---
+++ ExceptionLesson/NumberProcessService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionLesson
+{
+    public class NumberProcessService
+    {
+        /// <summary>
+        /// Math method with numbers
+        /// </summary>
+        /// <param name="potentialNumber">Argument</param>
+        /// <returns>Number</returns>
+        /// <exception cref="ArgumentException">ArgumentException</exception>
+        public bool IsNormalNumber(int potentialNumber)
+        {
+            
+            if (potentialNumber == 0)
+            {
+                return true;
+            }
+            else if (potentialNumber < 0)
+            {
+                throw new ArgumentException("Number less zero. Flu");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public double ModuleNumber(double Number)
+        {
+            return RootNumber(Number);
+        }
+
+        public double RootNumber(double Number)
+        {
+            if (Number>=0)
+                return Math.Sqrt(Number);
+            else
+            {
+                throw new ArgumentException("Number less zero.");
+            }
+        } 
+    }
+}
---
+++ ExceptionLesson/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ExceptionLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new NumberProcessService();
+            try
+            {
+                service.ModuleNumber(-16);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+            // try
+            // {
+            //    service.IsNormalNumber(-12);
+            // }
+            // catch (ArgumentException exception)
+            // {
+            //    Console.WriteLine(exception.Message);
+            // }
+
+
+            // int.TryParse(Console.ReadLine(), out var number);
+            // if (number > 10)
+            // {
+            //    Console.WriteLine("OK");
+            // }
+            // else
+            // {
+            //    throw new Exception("Number less 10!");
+            // } 
+
+            // var numbers = new int[] { 1, 2, 3, 4, 5 };
+            //// try-catch-finally, try-catch, try-finally
+            // try
+            // {
+            //    // numbers[5] = 12;
+            //    var number = int.Parse(Console.ReadLine());
+            // }
+            // catch(OverflowException exception)
+            // {
+            //    Console.WriteLine($"Very large number, we need use {int.MaxValue}");
+            // }
+            // catch(Exception exception)
+            // {
+            //    Console.WriteLine(exception.Message);
+            // }
+            // finally
+            // {
+            //    Console.WriteLine("Program finished");
+            // }
+        }
+    }
+}
